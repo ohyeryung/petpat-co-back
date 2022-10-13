c33bb5645ba7f6d0530cd14449371f53853be02c
kim-ju-ho <kim1jo2ho3@naver.com>
2022-10-14 02:45:08 +0900

    [feat] create project

---
+++ src/test/java/com/smile/petpat/PetpatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smile.petpat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetpatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
