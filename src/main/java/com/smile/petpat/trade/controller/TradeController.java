d974a9959a9ac5b714c31849c8e5492c46837136
kim-ju-ho <kim1jo2ho3@naver.com>
2022-11-11 02:13:31 +0900

    feat. create tradeController method

--- src/main/java/com/smile/petpat/trade/controller/TradeController.java
+++ src/main/java/com/smile/petpat/trade/controller/TradeController.java
@@ -2,12 +2,16 @@ package com.smile.petpat.trade.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/api/v1/trade")
 public class TradeController {
 
+    @RequestMapping(value = "/",method = RequestMethod.POST)
+    public void registerTrade(){
 
+    }
 }
