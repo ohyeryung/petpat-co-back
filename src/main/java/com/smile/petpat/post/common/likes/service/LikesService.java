1e83a1426dcf3970b6df0f49bb86b2b41aa26622
ohyeryung <dpfu1948@g.eulji.ac.kr>
2023-01-18 17:32:23 +0900

    refactor : Likes, Bookmark service interface 추가 구현

---
+++ src/main/java/com/smile/petpat/post/common/likes/service/LikesService.java
@@ -0,0 +1,9 @@
+package com.smile.petpat.post.common.likes.service;
+
+import com.smile.petpat.user.domain.User;
+
+import java.util.HashMap;
+
+public interface LikesService {
+    HashMap<String, String> likePost(Long postId, String postType, User user);
+}
