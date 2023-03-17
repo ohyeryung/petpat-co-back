b07c0d8571e21ffd309937a2ffd638c1c0a86139
kimjuho <kim1jo2ho3@naver.com>
2023-03-17 17:37:14 +0900

    feat : swagger config 추가
    swagger 이용을 위한 config 추가 
    spring 버전 과 swagger 버전과의 버그로 yml에 
    '  spring : 
          mvc:
                  path match:
                       matching-strategy: ant_path_matcher''' 추가 필수

---
+++ src/main/java/com/smile/petpat/config/SwaggerConfig.java
@@ -0,0 +1,99 @@
+package com.smile.petpat.config;
+
+import org.apache.http.HttpHeaders;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
+import org.springframework.boot.actuate.endpoint.web.*;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    private String version;
+    private String title;
+
+
+    @Bean
+    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier
+            , ServletEndpointsSupplier servletEndpointsSupplier
+            , ControllerEndpointsSupplier controllerEndpointsSupplier
+            , EndpointMediaTypes endpointMediaTypes
+            , CorsEndpointProperties corsProperties
+            , WebEndpointProperties webEndpointProperties
+            , Environment environment) {
+        List<ExposableEndpoint<?>> allEndpoints = new ArrayList();
+        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+        allEndpoints.addAll(webEndpoints);
+        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+        String basePath = webEndpointProperties.getBasePath();
+        EndpointMapping endpointMapping = new EndpointMapping(basePath);
+        boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment, basePath);
+        return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes, corsProperties.toCorsConfiguration()
+                , new EndpointLinksResolver(allEndpoints, basePath), shouldRegisterLinksMapping, null);
+    }
+
+
+    private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment, String basePath) {
+        return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath) || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
+    }
+    @Bean
+    public Docket apiV1() {
+        version = "V1";
+        title = "Swagger Example API " + version;
+
+//        Parameter parameterBuilder = new ParameterBuilder()
+//                .name(HttpHeaders.AUTHORIZATION)
+//                .description("Access Token")
+//                .modelRef(new ModelRef("string"))
+//                .parameterType("header")
+//                .required(false)
+//                .build();
+//
+//        List<Parameter> globalParameters = new ArrayList<>();
+//        globalParameters.add(parameterBuilder);
+
+        return new Docket(DocumentationType.SWAGGER_2)
+//                .globalOperationParameters(globalParameters)
+                .useDefaultResponseMessages(false)
+                .groupName(version)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo(title, version));
+
+    }
+
+    private ApiInfo apiInfo(String title, String version) {
+        return new ApiInfoBuilder()
+                .title(title)
+                .description("Swagger로 생성한 API Docs")
+                .version(version)
+                .build();
+
+
+    }
+}
