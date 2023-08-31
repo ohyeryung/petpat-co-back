fab58cf78be14a47211af94edfcdb0aa466b4382
littlecold4 <minu09252@gmail.com>
2023-08-31 15:42:37 +0900

    feat : 회원가입_닉네임 중복 검사

--- src/main/java/com/smile/petpat/user/domain/UserReader.java
+++ src/main/java/com/smile/petpat/user/domain/UserReader.java
@@ -5,6 +5,7 @@ import com.smile.petpat.user.dto.SocialUserDto;
 
 public interface UserReader {
     void getUserByUserEmail(String userEmail);
+    void getUserByNickName(String nickName);
     void getUser(User user);
 
     SocialUserDto getKakaoUserInfo(String accessToken) throws JsonProcessingException;
